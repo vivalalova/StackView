e0e433323abfa741594026b2abbc25fbd56f0339
vivalalova <vivalalova0@gmail.com>
2020-04-25 16:18:32 +0800

    fix

--- Package.swift
+++ Package.swift
@@ -5,24 +5,19 @@ import PackageDescription
 
 let package = Package(
     name: "StackView",
+    platforms: [
+        .iOS(SupportedPlatform.IOSVersion.v10)
+    ],
     products: [
-        // Products define the executables and libraries produced by a package, and make them visible to other packages.
-        .library(
-            name: "StackView",
-            targets: ["StackView"]),
+        .library(name: "StackView", targets: ["StackView"]),
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-         .package(url: "https://github.com/SnapKit/SnapKit", from: "5.0.1"),
+        .package(url: "https://github.com/SnapKit/SnapKit", from: "5.0.1"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
-        .target(
-            name: "StackView",
-            dependencies: []),
-        .testTarget(
-            name: "StackViewTests",
-            dependencies: ["StackView"]),
+        .target(name: "StackView", dependencies: ["SnapKit"]),
+        .testTarget(name: "StackViewTests", dependencies: ["StackView"]),
     ]
 )
--- Tests/StackViewTests/StackViewTests.swift
+++ Tests/StackViewTests/StackViewTests.swift
@@ -6,7 +6,7 @@ final class StackViewTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(StackView().text, "Hello, World!")
+        XCTAssertEqual(StackView().isAutoAxis, false)
     }
 
     static var allTests = [
